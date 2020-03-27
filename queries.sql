634d93d559f5334edac5c23e088ef88760576ba2
filipe-gomes <filipegomes404@gmail.com>
2020-03-26 23:41:16 -0500

    Completed Assignment #4

--- queries.sql
+++ queries.sql
@@ -1,7 +1,20 @@
 ## Part 1: Test it with SQL
+SELECT column_name, data_type
+FROM information_schema.columns
+WHERE table_name = 'job'
+ORDER BY ordinal_position;
 
 ## Part 2: Test it with SQL
+SELECT name
+FROM employer
+WHERE location = "St. Louis City";
 
 ## Part 3: Test it with SQL
+DROP TABLE job;
 
 ## Part 4: Test it with SQL
+SELECT DISTINCT name, description
+FROM skill
+INNER JOIN job_skills ON skill.id=job_skills.skills_id
+WHERE jobs_id IS NOT NULL
+ORDER BY name;
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/HomeController.java
@@ -1,6 +1,12 @@
 package org.launchcode.javawebdevtechjobspersistent.controllers;
 
+import org.launchcode.javawebdevtechjobspersistent.models.Employer;
 import org.launchcode.javawebdevtechjobspersistent.models.Job;
+import org.launchcode.javawebdevtechjobspersistent.models.Skill;
+import org.launchcode.javawebdevtechjobspersistent.models.data.EmployerRepository;
+import org.launchcode.javawebdevtechjobspersistent.models.data.JobRepository;
+import org.launchcode.javawebdevtechjobspersistent.models.data.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -8,44 +14,82 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by LaunchCode
  */
 @Controller
+@RequestMapping("/")
 public class HomeController {
+
+    @Autowired
+    private EmployerRepository employerRepository;
+
+    @Autowired
+    private SkillRepository skillRepository;
+
+    @Autowired
+    private JobRepository jobRepository;
 
     @RequestMapping("")
     public String index(Model model) {
-
         model.addAttribute("title", "My Jobs");
-
+        model.addAttribute("jobs", jobRepository.findAll());
         return "index";
     }
 
     @GetMapping("add")
     public String displayAddJobForm(Model model) {
         model.addAttribute("title", "Add Job");
+        model.addAttribute("employers", employerRepository.findAll());
+        model.addAttribute("skills", skillRepository.findAll());
         model.addAttribute(new Job());
         return "add";
     }
 
-    @PostMapping("add")
+    @PostMapping ("add")
     public String processAddJobForm(@ModelAttribute @Valid Job newJob,
-                                       Errors errors, Model model, @RequestParam int employerId, @RequestParam List<Integer> skills) {
-
+                                    Errors errors, Model model, @RequestParam int employerId,
+                                    @RequestParam List<Integer> skills) {
         if (errors.hasErrors()) {
             model.addAttribute("title", "Add Job");
+            model.addAttribute("employers", employerRepository.findAll());
+            model.addAttribute("skills", skillRepository.findAll());
+            return "add";
+        }
+        Optional <Employer> employerRepo = employerRepository.findById(employerId);
+        Employer employer = employerRepo.get();
+        newJob.setEmployer(employer);
+        if (employerRepo.isEmpty()) {
+            model.addAttribute("title", "No Employer matching criteria has been found");
+            return "add";
+        }
+        if (skills.size() > 0) {
+            List<Skill> skillsList = (List<Skill>) skillRepository.findAllById(skills);
+            newJob.setSkills(skillsList);
+            jobRepository.save(newJob);
+            return "redirect:";
+        } else {
+            model.addAttribute("title", "Add Job");
+            model.addAttribute("employers", employerRepository.findAll());
+            model.addAttribute("skills", skillRepository.findAll());
             return "add";
         }
 
-        return "redirect:";
     }
 
     @GetMapping("view/{jobId}")
     public String displayViewJob(Model model, @PathVariable int jobId) {
 
-        return "view";
+        Optional <Job> jobRepo = jobRepository.findById(jobId);
+        if (jobRepo.isPresent()) {
+            Job job = (Job) jobRepo.get();
+            model.addAttribute("job", job);
+            return "view";
+        } else {
+            return "redirect";
+        }
     }
 
 
